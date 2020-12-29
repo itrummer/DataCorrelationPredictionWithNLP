5af2d2e25438d76ea6d82df5abf903189889a020
itrummer <immanuel.trummer@gmail.com>
2020-12-29 23:10:04 +0000

    updated data preparation scripts, fixed a few bugs

--- dataprep/analyzeall.sh
+++ dataprep/analyzeall.sh
@@ -1,2 +1,3 @@
-find . -name '*.csv' > allcsvs
-let 'x=0'; while read f; do let 'x=x+1'; python3 coranalyze.py $f $x >> corresult2.csv; done < allcsvs
+find ../data -name '*.csv' > ../data/allcsvs
+rm ../data/corresult2.csv
+let 'x=0'; while read f; do let 'x=x+1'; python3 coranalyze.py "$f" $x >> ../data/corresult2.csv; done < ../data/allcsvs
--- dataprep/coranalyze.py
+++ dataprep/coranalyze.py
@@ -5,13 +5,19 @@ import sys
 path = sys.argv[1]
 dsid = sys.argv[2]
 
-# read and factorize data
+# read, cut, and factorize data
+#print(f'Reading data from path {path}', flush=True)
 data = pd.read_csv(path)
+nr_cols = len(data.columns)
+if nr_cols > 10:
+    data = data.iloc[:,range(0,10)]
 fdata = data.apply(lambda x: x.factorize()[0])
 
 # get basic info on columns
 types = data.dtypes
+#print(f'Types: {types}', flush=True)
 nr_cols = len(data.columns)
+#print(f'Nr. columns: {nr_cols}', flush=True)
 
 # calculate column stats
 nr_vals = []
@@ -19,6 +25,7 @@ for c in range(0, nr_cols):
     counts = data.iloc[:,c].value_counts()
     nr_vals.append(len(counts))
 nr_rows = len(data.index)
+#print(f'Nr. rows: {nr_rows}')
 
 # iterate over correlation metrics
 for m in ['pearson', 'kendall', 'spearman']:
@@ -33,5 +40,5 @@ for m in ['pearson', 'kendall', 'spearman']:
             nrv1 = nr_vals[i]
             nrv2 = nr_vals[j]
             cur_cor = cor.iloc[i,j]
-            print(f'{dsid},{path},{m},{nr_rows},{nrv1},{nrv2},{type1},{type2},{col1},{col2},{cur_cor}')
+            print(f'{dsid},"{path}",{m},{nr_rows},{nrv1},{nrv2},{type1},{type2},"{col1}","{col2}",{cur_cor}')
 
