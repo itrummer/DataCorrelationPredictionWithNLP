53f11e3777b139580eb2b9119b90cdc4a9b26820
itrummer <immanuel.trummer@gmail.com>
2021-01-08 06:28:22 +0000

    added new results

--- predict/analyze.sh
+++ predict/analyze.sh
@@ -1,15 +1,13 @@
 for coef in pearson spearman theilsu; do
-	for mod in "roberta roberta-base" "bert bert-base-cased" "distilbert distilbert-base-cased" "albert albert-base-v2" "xlm xml-mlm-en-2048"; do
-		for scenario in defsep; do
-			echo $coef $mod $scenario;
-			python3 coefficients.py $coef 0.5 1 $mod $scenario &> $coef"$mod"$scenario.log
+	for mod in "roberta roberta-base" "distilbert distilbert-base-cased" "albert albert-base-v2"; do
+		for scenario in defsep datasep; do
+			for limits in "0.25 1" "0.5 1" "0.75 1" "0.5 0.05"; do 
+				for testratio in "0.2" "0.8"; do
+					echo $coef $mod $scenario $limits $testratio
+					python3 coefficients.py $coef $limits $mod $scenario $testratio
+				done;
+			done;
 		done;
 	done;
 done
-#python3 coefficients.py pearson 0.5 1 &> ac1.log
-#python3 coefficients.py spearman 0.5 1 &> ac2.log
-#python3 coefficients.py theilsu 0.5 1 &> ac3.log
-#python3 coefficients.py pearson 0.5 0.05 &> ac4.log
-#python3 coefficients.py spearman 0.5 0.05 &> ac5.log
-#python3 coefficients.py theilsu 0.5 0.5 &> ac6.log
 
